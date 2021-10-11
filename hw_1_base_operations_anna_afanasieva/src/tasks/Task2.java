bd7d11bd0b5e70afaeb4342d80bd7b59827c3448
AnnaAfanasieva <68120022+AnnaAfanasieva@users.noreply.github.com>
2021-10-11 12:31:15 +0300

    Completed Task2, update start_hw_1 for Task2

--- hw_1_base_operations_anna_afanasieva/src/tasks/Task2.java
+++ hw_1_base_operations_anna_afanasieva/src/tasks/Task2.java
@@ -1,7 +1,49 @@
 package tasks;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task2 {
+
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String string = in.next();
+        sortArray(string);
+
     }
+
+    private static void sortArray(String string) {
+        char[] arrFromString = string.toCharArray();
+
+        //Удаление ненужных символов
+        for (int i = 0; i < arrFromString.length; i++) {
+            if (arrFromString[i] < 65 || (arrFromString[i] > 90 && arrFromString[i] < 97) || (arrFromString[i] > 122 && arrFromString[i] < 1040) || arrFromString[i] > 1103) {
+                arrFromString[i] = 0;
+            }
+        }
+
+        //Сортировка массива
+        Arrays.sort(arrFromString);
+
+        //Цикл подсчета и вывода результата на экран
+        for (int i = 0; i < arrFromString.length; i++) {
+            if (arrFromString[i] != 0) {
+                int counter = 0;
+                int index = i;
+                int j = i;
+                while (j != arrFromString.length && arrFromString[j] == arrFromString[index]) {
+                    counter++;
+                    j++;
+                }
+
+                System.out.println(arrFromString[i] + " - " + counter);
+                i = j - 1;
+
+            }
+        }
+
+    }
+
 }
