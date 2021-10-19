472e4c5d472b0166d6ed5b093728f66e77b6fe68
AnnaAfanasieva <aniafanasieva24@gmail.com>
2021-10-20 00:43:20 +0300

    add second person

--- hw_2/src/SomeClass.java
+++ hw_2/src/SomeClass.java
@@ -1,7 +1,7 @@
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("something");
-        Person person = new Person();
+        Person person = new Person("",0);
         person.setName("John");
         person.setAge(134);
         System.out.println(person.toString());
