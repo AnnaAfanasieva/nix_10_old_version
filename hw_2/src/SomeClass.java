eb43c8dd50f583d317e15d6a4821c0685379172d
AnnaAfanasieva <aniafanasieva24@gmail.com>
2021-10-20 00:12:36 +0300

    add Person and created example

--- hw_2/src/SomeClass.java
+++ hw_2/src/SomeClass.java
@@ -1,6 +1,10 @@
 public class SomeClass {
     public static void main(String[] args) {
         System.out.println("something");
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(34);
+        person.toString();
 
     }
 }
